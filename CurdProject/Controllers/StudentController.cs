4292a39e4279a85150cee9c9563ee9d392902e5c
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-16 13:13:04 +0530

    Create controller

---
+++ CurdProject/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using CurdProject.DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurdProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+    }
+}
