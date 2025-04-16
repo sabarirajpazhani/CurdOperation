8cd16268d371c4a15277f66d6363b1e5fdcc2218
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-16 12:25:53 +0530

    Add project files.

---
+++ CurdProject/DB/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CurdProject.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurdProject.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Student> students { get; set; }    
+    }
+}
---
+++ CurdProject/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CurdProject.Model
+{
+    public class Student   //Property or Model or Entity
+    {
+        [Key]
+        public int Id { get; set; } //Primary Key 
+        [Required]
+        public string Name { get; set; } = null!;
+        public string? Address { get; set; }
+        public int PhoneNo { get; set; }    
+
+    }
+}
